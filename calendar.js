3fd86c15a118218fd30567314c24aa2273fca1d6
JStrummer <64981369+JStrummer@users.noreply.github.com>
2020-11-21 04:03:19 +0100

    Initial commit

---
+++ calendar.js
@@ -0,0 +1,4 @@
+'use strict'
+
+var canvas = document.querySelector('#calendar');
+var ctx = canvas.getContext('2d');
---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// global reference
+var currentDate = document.querySelector('#current-date');
+var countdown = document.querySelector('#countdown');
+
+updateTimer();
+animBackground();
