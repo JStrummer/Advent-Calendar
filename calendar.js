21fe1d6dab70c66a994fd891e6b6f870072e96f4
JStrummer <pescosolidopaolo@gmail.com>
2020-11-22 02:54:58 +0100

    Created grid layout

--- calendar.js
+++ calendar.js
@@ -1,4 +1,24 @@
 'use strict'
 
-var canvas = document.querySelector('#calendar');
-var ctx = canvas.getContext('2d');
+// global reference
+var calendar = document.querySelector("#calendar");
+
+var boxes = [];
+
+//create boxes
+for (let i = 1; i < 25; i++) {
+  var boxElement = document.createElement("div");
+  calendar.appendChild(boxElement);
+  boxElement.classList.add('box');
+  boxes.push(box(boxElement, i));
+ }
+
+function box (element, date) {
+  var date = date;
+  element.textContent = date;
+  
+  return {
+    date: date,
+    el: element
+  }
+}
