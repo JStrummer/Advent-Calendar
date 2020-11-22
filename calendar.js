d71f4dfbe849f37d40dbd96d8bcc05e2180c5d3f
JStrummer <64981369+JStrummer@users.noreply.github.com>
2020-11-22 19:17:49 +0100

    changed from canvas to grid

--- calendar.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-var canvas = document.querySelector('#calendar');
-var ctx = canvas.getContext('2d');
---
+++ data/html-element.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var calendar = document.querySelector("#calendar");
+var currentDate = document.querySelector('#current-date');
+var countdown = document.querySelector('#countdown');
+var greetings = document.querySelector('#greetings');
+var submitNameForm = document.querySelector('form');
+var submitNameBtn = document.querySelector('#submitNameBtn');
+var inputName = document.querySelector('input#name');
