10053eb56e14070e2d74d6cd8f945a28ad100c23
JStrummer <64981369+JStrummer@users.noreply.github.com>
2020-11-30 04:46:58 +0100

    joined box.js in calendar.js

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,10 +1,89 @@
 'use strict'
 
-var boxes = [];
+function box (element, date) {
+  var date = new Date(2020, 10, date);
+  var day = date.getDate();
+  var isOpen = false;
+  var isLocked = true;
+  var gift = gifts.get(day);
+  element.dataset.date = day;
+  // setting image for the date
+  var dateImage = document.createElement('img');
+  dateImage.src = "graphic/img/dates/" + day + ".png";
+  dateImage.classList.add('date');
+  // setting image for the gift when box is open
+  var previewGift = document.createElement('img');
+  previewGift.classList.add('previewGift');
+  previewGift.src = gift.preview;
 
-//create boxes
-for (let i = 1; i < 25; i++) {
-  let boxElement = document.createElement("div");
-  boxElement.classList.add('box');
-  boxes.push(box(boxElement, i));
- }
+  element.appendChild(dateImage);
+  element.appendChild(previewGift);
+
+  return {
+    get gift () {
+      return gift;
+    },
+    get isOpen () {
+      return isOpen;
+    },
+    get isLocked () {
+      return isLocked;
+    },
+    get date() {
+      return date;
+    },
+    get element () {
+      return element;
+    },
+    unlock () {
+      if (isLocked && checkDate()) {
+        isLocked = false;
+        this.element.classList.add('unlocked');
+        this.element.addEventListener('click', openBox);
+        return true;
+      }
+      return false;
+      // check if you can open the box
+      function checkDate () {
+        if (Date.now() >= date) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    open () {
+      if (!isLocked && !isOpen) {
+        isOpen = true;
+        this.element.classList.add('open');
+        this.element.classList.remove('unlocked');
+        // change image to show the gift
+        dateImage.style.display = 'none';
+        previewGift.classList.add('show');
+        this.element.removeEventListener('click', openBox);
+        this.element.addEventListener('click', openGift);
+        return true;
+      }
+      return false;
+    }
+  }
+  function openBox (evt) {
+    if (state.saved) {
+      // open box
+      var box = evt.currentTarget;
+      var index = box.dataset.date - 1;
+      boxes[index].open();
+
+      state.save();
+
+      box.removeEventListener('click', openBox);
+      box.addEventListener('click', openGift);
+    }
+  }
+
+  function openGift (evt) {
+    var box = evt.currentTarget;
+    var index = box.dataset.date - 1;
+    boxes[index].gift.show();
+  }
+}
--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 'use strict'
 
+// getting html reference
 var calendar = document.querySelector("#calendar");
 var submitNameBtn = document.querySelector('#submitNameBtn');
 var inputName = document.querySelector('input#name');
+// create state instance
 var state = fetchState();
-
+// create box objects
+var boxes = [];
+for (let i = 1; i < 25; i++) {
+  let boxElement = document.createElement("div");
+  boxElement.classList.add('box');
+  boxes.push(box(boxElement, i));
+}
+// adding boxes elements to calendar
 boxes.forEach((box, i) => {
   calendar.appendChild(box.element);
 });
-
-
+// checking if exists data from previous sections
 if (state.getData()) {
+  // if data exists
   updateGreetings(state.userName);
-  let savedBox = state.saved;
   boxes.forEach( (box, i) => {
     if (box.unlock()) {
       addSound('click', box.element, document.querySelector('#gliss'));
     }
-
-    if (savedBox[i].open) {
+    if (state.saved[i].open) {
       box.open();
     }
   });
 } else {
+  // if data dont exists
   submitNameBtn.addEventListener('click', submitName);
 
   function submitName (evt) {
+    // get name from Form
     var name = inputName.value;
     if (name) {
       let data = {userName: name, saved: []};
