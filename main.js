2a7ccf2b4c6d9bcd424a1f975e954c8ae0e83d7a
JStrummer <64981369+JStrummer@users.noreply.github.com>
2020-11-30 05:26:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict'
 
+// added localStorage.clear for mobile
+var greetings = document.querySelector('#greetings');
+greetings.addEventListener('click', () => localStorage.clear());
+
 // getting html reference
 var calendar = document.querySelector("#calendar");
 var submitNameBtn = document.querySelector('#submitNameBtn');
